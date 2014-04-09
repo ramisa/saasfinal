09d54b0a7daf81540c86a72e8bd7b5906d08e9d7
saasbook <saasbook@saasbook.(none)>
2014-04-08 21:18:16 -0300

    More changes to DB..again

---
+++ books_app/app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+
+
+  def index
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering,@title_header = {:order => :title}, 'hilite'
+    when 'author'
+      ordering,@author_header = {:order => :release_date}, 'hilite'
+    end
+    @condition = Book.conditions
+    @price = Book.price
+    
+    
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      session[:sort] = sort
+
+      redirect_to :sort => sort
+    end
+   
+
+  def new
+    # default: render 'new' template
+  end
+
+  def create
+	
+    @book = book.new(params[:book])
+	if @book.save
+
+    flash[:notice] = "#{@book.title} was successfully created."
+    redirect_to books_path
+else
+
+	render: new
+  end
+
+  def update
+    @book.update_attributes!(params[:book])
+    flash[:notice] = "#{@book.title} was successfully updated."
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    @book = book.find(params[:title])
+    @book.destroy
+    flash[:notice] = "Book '#{@book.title}' deleted."
+    redirect_to books_path
+  end
+
+end
